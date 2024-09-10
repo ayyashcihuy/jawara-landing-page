26e2ad9260becbbe70213986359afddb3c61ff85
ayyashcihuy <ayyashcihuy@gmail.com>
2024-09-10 23:49:53 +0700

    change lp to sks

--- src/app/(Sections)/Section2.tsx
+++ src/app/(Sections)/Section2.tsx
@@ -1,47 +1,38 @@
-import ImageCarouselContainer from "@/components/ImageCarousel/ImageCarousel";
+import { Della_Respira } from "next/font/google";
 import { Content } from "@/components/LayersDescription/Description";
 import LayersDescription from "@/components/LayersDescription/LayersDescription";
 
-export default function Section2() {
-    const imageSection = [
-        "/assets/example1.svg", "/assets/example2.svg", "/assets/example3.svg"
-    ]
+const della_respira = Della_Respira({
+    subsets: ["latin"],
+    weight: "400",
+    style: "normal",
+});  
 
+export default function Section2() {
     const contents: Content[] = [
         {
-            image: "/assets/ic_layers_48px.svg",
-            title: "Pilihan Template",
-            description: "Anda dapat memilih beragam design website yang sudah kami sediakan dan dapat Anda gunakan"
+            image: "/assets/icon/paper.svg",
+            description: "Sudah bersifat sertifikat hak milik"
         },
         {
-            image: "/assets/chart-bar-33.svg",
-            title: "Responsive Design",
-            description: "Website yang Anda akan dapat sudah memiliki fitur responsive design untuk dapat diakses dimanapun"
+            image: "/assets/icon/furniture.svg",
+            description: "Seluruh unit kamar fully furnished"
         },
         {
-            image: "/assets/a-sync.svg",
-            title: "Konsultasi Gratis",
-            description: "Jangan takut kebingungan, Anda dapat berkonsultasi dengan tim Kami untuk mewujudukan website impian Anda"
+            image: "/assets/icon/kulkas.svg",
+            description: "Memiliki beragam fasilitas penunjang untuk penghuni"
         }
     ]
 
     return (
         <div className="w-full bg-white p-8 py-16 flex flex-col gap-y-8">
-            <div className="flex flex-row gap-x-8 items-center justify-center">
-                {
-                    imageSection.map((item, index) => {
-                        return(
-                            <ImageCarouselContainer src={item} key={index} />
-                        )
-                    })
-                }
-            </div>
             <div className="mt-10 flex flex-col gap-y-16">
-                <div className="flex flex-col gap-y-6 w-full items-center">
-                    <h1 className="font-bold text-[36px] text-center">Kami siap membantu Anda</h1>
-                    <p className="text-center max-w-[438px]">
-                        Wujudkan website yang profesional sesuai dengan kebutuhan
-                        dari produk yang ingin Anda pasarkan
+                <div className="flex flex-col gap-y-3 w-full items-center">
+                    <div className="font-bold text-[36px] text-center">
+                        <h1 className={della_respira.className}>Kenapa Harus Milih SKS?</h1>
+                    </div>
+                    <p className="text-center">
+                        SKS Living memiliki 6 keunggulan yang dapat menunjang kehidupan kamu
                     </p>
                 </div>
                 
